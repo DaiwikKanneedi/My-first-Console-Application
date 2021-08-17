5cbbaa2a17792efe2df9a642f6b074272b81abaa
DaiwikKanneedi <76936261+DaiwikKanneedi@users.noreply.github.com>
2021-08-17 16:01:32 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Daiwik__First_Console_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Title = "Daiwik's First Console";
+            Console.ForegroundColor= ConsoleColor.White;
+            Console.WriteLine("Hello Whats Your Name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello "+ userName+" Nice to meet You");
+            Console.ReadLine();
+            Console.WriteLine("so What's Your Intreasted In?");
+            string IntreastedIn = Console.ReadLine();
+            Console.WriteLine("OH HO HO HO "+IntreastedIn+" hmmmm I Don't Know "+IntreastedIn+" So I think I cannot help you");  
+            Console.WriteLine("Message If you Press any Key The console will be close");
+            Console.ReadKey();
+        }
+    }
+}
